8956a6e2d71a020756cf28f16b7561bfc9975ea9
vijpandaturtle <thisisvij98@gmail.com>
2017-03-25 15:38:45 +0530

    MVVM

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+var Locations = [
+        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+];
+
+
 var map;
 var infowindow;
 var latlngbounds;
@@ -8,22 +18,13 @@ function initMap() {
   zoom: 13
 });
 
-var locations = [
-        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-      ];
-
 infowindow = new google.maps.InfoWindow();
 latlngbounds = new google.maps.LatLngBounds();
 
-for(i=0; i<locations.length; i++) {
+for(i=0; i<Locations.length; i++) {
 
-  var position = locations[i].location;
-  var title = locations[i].title;
+  var position = Locations[i].location;
+  var title = Locations[i].title;
 
   var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
 
